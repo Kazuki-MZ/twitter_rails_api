b7e579ed8154c7630cee46fc6fd90d8ec280ca47
kazu1212-star <12matumatu04@gmail.com>
2023-11-05 17:15:14 +0900

    user情報を取得

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class UsersController < ApplicationController
+      before_action :offset_params, only: [:show]
+
+      def show
+        user = User.find(params[:id])
+        user_tweets = user.tweets.order(created_at: 'DESC').includes(%i[user tweet_image]).limit(5).offset(@offset_params)
+        render json: { userInfo: user, tweets: user_tweets.as_json(include: :user, methods: :image_url),
+                       profile: user.profile.as_json(methods: %i[header_image_url icon_image_url]) }
+      end
+
+      private
+
+      def offset_params
+        @offset_params = params[:offset]&.to_i
+      end
+    end
+  end
+end
